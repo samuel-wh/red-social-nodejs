bac0d23056cc36e748166d6c4e663b62cf49fe46
samuel-wh <samuel@wisphub.net>
2023-09-07 10:58:23 -0500

    Add auth

--- store/dummy.js
+++ store/dummy.js
@@ -1,29 +1,19 @@
-const db = {
-  'user': [
-    { id: '1', name: 'Carlos' },
-  ],
-};
-
+const db = { user: [{ id: "1", name: "Carlos" }] };
 async function list(tabla) {
   return db[tabla];
 }
-
 async function get(tabla, id) {
   let col = await list(tabla);
-  return col.filter(item => item.id === id)[0] || null;
+  return col.filter((item) => item.id === id)[0] || null;
 }
-
 async function upsert(tabla, data) {
+  if (!db[tabla]) {
+    db[tabla] = [];
+  }
   db[tabla].push(data);
+  console.log(db);
 }
-
 async function remove(tabla, id) {
   return true;
 }
-
-module.exports = {
-  list,
-  get,
-  upsert,
-  remove,
-};
+module.exports = { list, get, upsert, remove };
