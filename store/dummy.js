cfc0d38af4c830c91145dbc7456cc17ce211093a
samuel-wh <samuel@wisphub.net>
2023-09-07 10:33:29 -0500

    Add documentacion y crud users

--- store/dummy.js
+++ store/dummy.js
@@ -1,15 +1,29 @@
-const db = { user: [{ id: "1", name: "Carlos" }] };
+const db = {
+  'user': [
+    { id: '1', name: 'Carlos' },
+  ],
+};
+
 async function list(tabla) {
   return db[tabla];
 }
+
 async function get(tabla, id) {
   let col = await list(tabla);
-  return col.filter((item) => item.id === id)[0] || null;
+  return col.filter(item => item.id === id)[0] || null;
 }
+
 async function upsert(tabla, data) {
-  db[collection].push(data);
+  db[tabla].push(data);
 }
+
 async function remove(tabla, id) {
   return true;
 }
-module.exports = { list, get, upsert, remove };
+
+module.exports = {
+  list,
+  get,
+  upsert,
+  remove,
+};
