386c1d55320258929b1341dc7eb278454de4f76a
samuel-wh <samuel@wisphub.net>
2023-09-07 13:29:40 -0500

    Base de datos real: MySQL

--- config.js
+++ config.js
@@ -1,8 +1,10 @@
 module.exports = {
-  api: {
-    port: process.env.API_PORT || 3000,
+  api: { port: process.env.API_PORT || 3000 },
+  jwt: { secret: process.env.JWT_SECRET || "notasecret!" },
+  mysql: {
+    host: process.env.MYSQL_HOST || "localhost",
+    user: process.env.MYSQL_USER || "root",
+    password: process.env.MYSQL_PASS || "root",
+    database: process.env.MYSQL_DB || "redSocial",
   },
-  jwt: {
-    secret: process.env.JWT_SECRET || 'notasecret!',
-  },
-}
+};
