9bf6d0f38f8b1b6437b90e995f67e1be2455ea81
samuel-wh <samuel@wisphub.net>
2023-09-07 23:16:58 -0500

    Completando la base de datos

--- store/mysql.js
+++ store/mysql.js
@@ -5,7 +5,7 @@ const dbconf = {
   user: config.mysql.user,
   password: config.mysql.password,
   database: config.mysql.database,
-
+  port: "3307"
 };
 let connection;
 function handleCon() {
@@ -28,7 +28,7 @@ function handleCon() {
   });
 }
 handleCon();
-function list(table, id) {
+function list(table) {
   return new Promise((resolve, reject) => {
     connection.query(`SELECT * FROM ${table}`, (err, data) => {
       if (err) return reject(err);
@@ -36,4 +36,56 @@ function list(table, id) {
     });
   });
 }
-module.exports = { list };
+function get(table, id) {
+  return new Promise((resolve, reject) => {
+    connection.query(
+      `SELECT * FROM ${table} WHERE id=${id}`,
+      (err, data) => {
+        if (err) return reject(err);
+        resolve(data);
+      }
+    );
+  });
+}
+function insert(table, data) {
+  return new Promise((resolve, reject) => {
+    connection.query(`INSERT INTO ${table} SET ?`, data, (err, result) => {
+      if (err) return reject(err);
+      resolve(result);
+    });
+  });
+}
+function update(table, data) {
+  return new Promise((resolve, reject) => {
+    connection.query(
+      `UPDATE ${table} SET ? WHERE id=?`,
+      [data, data.id],
+      (err, result) => {
+        if (err) return reject(err);
+        resolve(result);
+      }
+    );
+  });
+}
+const upsert = async (table, payload) => new Promise((resolve, reject) => {
+  connection.query(`INSERT INTO ${table} SET ? ON DUPLICATE KEY UPDATE ?`, [payload, payload], (error, data) => {
+    console.log('UPDATE DATA: ', data)
+    if (error) {
+      return reject(error)
+    }
+    resolve(data)
+  })
+})
+function query(table, query) {
+  return new Promise((resolve, reject) => {
+    connection.query(
+      `SELECT * FROM ${table} WHERE ?`,
+      query,
+      (err, res) => {
+        if (err) return reject(err);
+        resolve(res[0] || null);
+      }
+    );
+  });
+}
+module.exports = { list, get, upsert, query };
