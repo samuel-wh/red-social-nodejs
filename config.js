b3fb457d3c74ecf8a0c8ca9b9a4670492d2c30c4
samuel-wh <samuel@wisphub.net>
2023-09-09 09:16:24 -0500

    Separando los posts a un microservicio

--- config.js
+++ config.js
@@ -1,5 +1,8 @@
 module.exports = {
   api: { port: process.env.API_PORT || 3000 },
+  post: {
+    port: process.env.POST_PORT || 3002,
+  },
   jwt: { secret: process.env.JWT_SECRET || "notasecret!" },
   mysql: {
     host: process.env.MYSQL_HOST || "localhost",
